0b4803107daa40775ec9302cdbe07112190d7d87
mathesukkj <kemuel.g7363@gmail.com>
2024-04-11 23:07:20 -0300

    feat: add Todo in db

--- server.go
+++ server.go
@@ -10,14 +10,22 @@ import (
 
 var db *gorm.DB
 
+type Todo struct {
+	gorm.Model
+	task   string
+	status bool
+}
+
 func main() {
 	e := echo.New()
 
 	dsn := "host=localhost user=postgres password=password dbname=todos port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
-	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("couldnt connect to database.")
 	}
+
+	db.AutoMigrate(&Todo{})
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, world!")

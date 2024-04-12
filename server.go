8b398340816b6432a41500e33998c981d328dd63
Matheus Silva <matheus.silva@in8.com.br>
2024-04-11 22:59:54 -0300

    feat: add connection to postgres db using gorm

--- server.go
+++ server.go
@@ -4,10 +4,20 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB
 
 func main() {
 	e := echo.New()
+
+	dsn := "host=localhost user=postgres password=password dbname=todos port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("couldnt connect to database.")
+	}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, world!")
